9d536ac6ded5804533cba36a32cd67063c6a8117
Dmytro Danko <dmitrij.danko@murka.com>
2019-01-07 20:54:25 +0200

    second commit

--- server/index.js
+++ server/index.js
@@ -1,9 +1,10 @@
 const express = require('express');
+const cors = require('cors');
 
 const app = express();
+app.use(cors());
 
 app.use(express.static('client'));
-app.use(express.static('files'));
 
 const PORT = process.env.PORT || 5000;
 
